6e566dd705a9a692d4542a0745a0d032a395d3b2
real-fht <nferhat20@gmail.com>
2023-04-03 11:11:57 +0100

    Finish mouse C api

--- capi/mouse.lua
+++ capi/mouse.lua
@@ -1,5 +1,4 @@
 ---@meta _
----@todo
 
 ---@class mouse.status
 ---@field x integer
@@ -8,16 +7,16 @@
 
 ---Manipulate and inspect the mouse cursor. The mouse buttons are represented as index.
 ---@class _mouse
----@field screen unknown
----@field current_client unknown
----@field current_wibox unknown
----@field current_widgets unknown
----@field current_widget unknown
----@field current_widget_geometry unknown
----@field current_widget_geometries unknown
----@field is_left_mouse_button_pressed unknown
----@field is_right_mouse_button_pressed unknown
----@field is_middle_mouse_button_pressed unknown
+---@field screen screen|nil # The screen under the cursor.
+---@field current_client client|nil # Get the client currently under the mouse cursor.
+---@field current_wibox wibox|nil # Get the wibox currently under the mouse cursor.
+---@field current_widgets wibox.widget[]|nil # Get the widgets currently under the mouse cursor.
+---@field current_widget wibox.widget|nil # Get the topmost widget currently under the mouse cursor.
+---@field current_widget_geometry geometry|nil # Get the current widget geometry.
+---@field current_widget_geometries geometry[]|nil # Get the current widget geometries.
+---@field is_left_mouse_button_pressed boolean # True if the left mouse button is pressed.
+---@field is_right_mouse_button_pressed boolean # True if the right mouse button is pressed.
+---@field is_middle_mouse_button_pressed boolean # True if the middle mouse button is pressed.
 local M
 
 ---Get the client or any object which is under the pointer.
