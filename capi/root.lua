7ce859238d461a4af619f201d209081f029e214c
real-fht <nferhat20@gmail.com>
2023-04-03 10:19:24 +0100

    Use "cursor" type from common.lua

--- capi/root.lua
+++ capi/root.lua
@@ -87,7 +87,7 @@ local M
 function M.fake_input(event_type, detail, x, y) end
 
 ---Set the root cursor.
----@param cursor_name "num_glyphs"|"arrow"|"based_arrow_down"|"based_arrow_up"|"boat"|"bogosity"|"bottom_left_corner"|"bottom_right_corner"|"bottom_side"|"bottom_tee"|"box_spiral"|"center_ptr"|"circle"|"clock"|"coffee_mug"|"cross"|"crosshair"|"cross_reverse"|"cursor"|"diamond_cross"|"dotbox"|"dot"|"double_arrow"|"draft_large"|"draft_small"|"draped_box"|"exchange"|"fleur"|"gobbler"|"gumby"|"hand"|"hand"|"heart"|"icon"|"iron_cross"|"leftbutton"|"left_ptr"|"left_side"|"left_tee"|"ll_angle"|"lr_angle"|"man"|"middlebutton"|"mouse"|"pencil"|"pirate"|"plus"|"question_arrow"|"rightbutton"|"right_ptr"|"right_side"|"right_tee"|"rtl_logo"|"sailboat"|"sb_down_arrow"|"sb_h_double_arrow"|"sb_left_arrow"|"sb_right_arrow"|"sb_up_arrow"|"sb_v_double_arrow"|"shuttle"|"sizing"|"spider"|"spraycan"|"star"|"target"|"tcross"|"top_left_arrow"|"top_left_corner"|"top_right_corner"|"top_side"|"top_tee"|"trek"|"ul_angle"|"umbrella"|"ur_angle"|"watch"|"xterm" # A X cursor name.
+---@param cursor_name cursor
 function M.cursor(cursor_name) end
 
 ---Get attached drawins to a screen.
